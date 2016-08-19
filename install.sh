60adfcef7289cc8a2fbb952db2c449af594ec1f3
Antho <antho@anthonix.fr>
2016-08-20 00:01:04 +0200

    Final install

--- install.sh
+++ install.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 EPEL=`echo ${DISTRO} | sed "s/centos-//"`
-[ -z $EPEL ] && rpm -ivh https://dl.fedoraproject.org/pub/epel/epel-release-latest-${EPEL}.noarch.rpm
+echo Downloading deps :
+rpm -ivh https://dl.fedoraproject.org/pub/epel/epel-release-latest-${EPEL}.noarch.rpm
 echo Downloading common deps :
 yum install wget git tar -y
-echo Downloading deps :
-rpm -ivh ${EPEL} 
