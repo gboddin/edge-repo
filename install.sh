68caa7d78b8b7460922301bdffcdd09f9dfce7fe
Antho <antho@anthonix.fr>
2016-08-19 23:58:03 +0200

    Revamp

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-EPEL=`sed "s/${DISTRO}-//"`
+EPEL=`echo ${DISTRO} | sed "s/centos-//"`
 [ -z $EPEL ] && rpm -ivh https://dl.fedoraproject.org/pub/epel/epel-release-latest-${EPEL}.noarch.rpm
 echo Downloading common deps :
 yum install wget git tar -y
