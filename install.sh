ac1834ca376cd192be25c384c4505b2731506ab3
Gregory Boddin <gboddin@users.noreply.github.com>
2016-08-20 00:19:42 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,4 +3,4 @@ EPEL=`echo ${DISTRO} | sed "s/centos-//"`
 echo Downloading deps :
 rpm -ivh https://dl.fedoraproject.org/pub/epel/epel-release-latest-${EPEL}.noarch.rpm
 echo Downloading common deps :
-yum install wget git tar -y
+yum install wget git tar rpm-build -y
