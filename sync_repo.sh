99dd9606fb2b4b996133ea16581bc8ff515b8d2a
Gregory Boddin <gregory@siwhine.net>
2016-08-20 18:00:35 +0200

    Fixed REPO variable

--- sync_repo.sh
+++ sync_repo.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-rsync -arvce "ssh -i deploy.key -o StrictHostKeyChecking=no" RPMS/$(uname -m)/ ${RSYNC_SERVER}:/var/www/html/repo/${DISTRO}/RPMS/$(uname -m)/
-rsync -arvce "ssh -i deploy.key -o StrictHostKeyChecking=no" SRPMS/ ${RSYNC_SERVER}:/var/www/html/repo/${DISTRO}/SRPMS/
+rsync -arvce "ssh -i deploy.key -o StrictHostKeyChecking=no" RPMS/$(uname -m)/ ${RSYNC_REPO}:/var/www/html/repo/${DISTRO}/RPMS/$(uname -m)/
+rsync -arvce "ssh -i deploy.key -o StrictHostKeyChecking=no" SRPMS/ ${RSYNC_REPO}:/var/www/html/repo/${DISTRO}/SRPMS/
