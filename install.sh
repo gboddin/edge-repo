0039b9203b26df9ef3a149b9593f84e28f9d5ae8
Gregory Boddin <gregory@siwhine.net>
2016-08-21 19:01:49 +0200

    Cleaned install.sh and using yum -y --nogpgcheck localinstall to install deps on localinstall

--- install.sh
+++ install.sh
@@ -3,5 +3,5 @@ EPEL=`echo ${DISTRO} | sed "s/centos-//"`
 echo Downloading deps :
 rpm -ivh https://dl.fedoraproject.org/pub/epel/epel-release-latest-${EPEL}.noarch.rpm
 echo Downloading common deps :
-yum install /sbin/service /etc/mime.types libdb wget git tar rpm-build rpmdevtools make -y
-[ ! -z ${ORACLE_OCI_RPM} ] && yum --nogpgcheck localinstall ${ORACLE_OCI_RPM}
+yum install wget git tar rpm-build rpmdevtools make -y
+[ ! -z ${ORACLE_OCI_RPM} ] && yum -y --nogpgcheck localinstall ${ORACLE_OCI_RPM}
