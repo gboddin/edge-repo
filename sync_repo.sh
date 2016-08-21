2086e2d2a7f00e0b5cf3a962651b16eb289ffe35
Gregory Boddin <gregory@siwhine.net>
2016-08-21 14:28:51 +0200

    Skip sync of already built pakcages

--- sync_repo.sh
+++ sync_repo.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-rsync -arvce "ssh -i deploy.key -o StrictHostKeyChecking=no" RPMS/$(uname -m)/ ${RSYNC_REPO}:/var/www/html/repo/${DISTRO}/RPMS/$(uname -m)/
-rsync -arvce "ssh -i deploy.key -o StrictHostKeyChecking=no" SRPMS/ ${RSYNC_REPO}:/var/www/html/repo/${DISTRO}/SRPMS/
+rsync --ignore-existing -arvce "ssh -i deploy.key -o StrictHostKeyChecking=no" RPMS/$(uname -m)/ ${RSYNC_REPO}:/var/www/html/repo/${DISTRO}/RPMS/$(uname -m)/
+rsync --ignore-existing -arvce "ssh -i deploy.key -o StrictHostKeyChecking=no" SRPMS/ ${RSYNC_REPO}:/var/www/html/repo/${DISTRO}/SRPMS/
