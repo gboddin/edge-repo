6f0e0b38a5863c2ef4f08477205fd94c6dbe424a
Gregory Boddin <gregory@siwhine.net>
2016-08-21 05:56:02 +0200

    Fix libgd install glob

--- build-php.sh
+++ build-php.sh
@@ -6,7 +6,7 @@ cat SPECS/php56.spec.tpl |sed "s/TPL_PHP_VERSION/$PHP_VERSION/g" > SPECS/php56.s
 echo $PHP_CONFIG_FLAGS|grep -q libgd && yum-builddep -y SPECS/gd.spec
 echo $PHP_CONFIG_FLAGS|grep -q libgd && wget "https://github.com/libgd/libgd/releases/download/gd-${GD_VERSION}/libgd-${GD_VERSION}.tar.xz" -cO SOURCES/libgd-${GD_VERSION}.tar.xz
 echo $PHP_CONFIG_FLAGS|grep -q libgd && rpmbuild --define "rpmrel 1" --define "_topdir `pwd`"  -ba SPECS/gd.spec > ${LOGFILE}
-echo $PHP_CONFIG_FLAGS|grep -q libgd && rpm -ivh RPMS/$(uname -m)/gd-last-{,devel-}${GD_VERSION}-1.el6.$(uname -m).rpm 
+echo $PHP_CONFIG_FLAGS|grep -q libgd && rpm -ivh RPMS/$(uname -m)/gd-last-{,devel-}${GD_VERSION}*.$(uname -m).rpm 
 
 spectool -g -C SOURCES SPECS/php56.spec
 yum-builddep -y SPECS/php56.spec
