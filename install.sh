df370d705d448df1faf28874deddb91f9fb34b6e
Gregory Boddin <gregory@siwhine.net>
2016-08-21 18:56:17 +0200

    Testing yum to install rpms

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-[ ! -z ${ORACLE_OCI_RPM} ] && rpm -ivh ${ORACLE_OCI_RPM}
 EPEL=`echo ${DISTRO} | sed "s/centos-//"`
 echo Downloading deps :
 rpm -ivh https://dl.fedoraproject.org/pub/epel/epel-release-latest-${EPEL}.noarch.rpm
 echo Downloading common deps :
 yum install /sbin/service /etc/mime.types libdb wget git tar rpm-build rpmdevtools make -y
+[ ! -z ${ORACLE_OCI_RPM} ] && yum --nogpgcheck localinstall ${ORACLE_OCI_RPM}
