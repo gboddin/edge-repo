3bc52a4c1c7d51fdf21975661a5eb602174dafaa
Gregory Boddin <gregory@siwhine.net>
2016-08-21 23:18:00 +0200

    Adding fedora 23 and 24 as build target

--- install.sh
+++ install.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-EPEL=`echo ${DISTRO} | sed "s/centos-//"`
 echo Downloading deps :
-rpm -ivh https://dl.fedoraproject.org/pub/epel/epel-release-latest-${EPEL}.noarch.rpm
+[ ! -z ${EPEL} } && rpm -ivh https://dl.fedoraproject.org/pub/epel/epel-release-latest-${EPEL}.noarch.rpm
 echo Downloading common deps :
 yum install wget git tar rpm-build rpmdevtools make -y
 [ ! -z ${ORACLE_OCI_RPM} ] && yum -y --nogpgcheck localinstall ${ORACLE_OCI_RPM}
