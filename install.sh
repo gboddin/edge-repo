3f93fe703f7c544b2a1746dddf1bc71072c8b103
Gregory Boddin <gregory@siwhine.net>
2016-08-22 03:35:12 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,4 +3,4 @@ echo Downloading deps :
 [ ! -z ${EPEL} ] && rpm -ivh https://dl.fedoraproject.org/pub/epel/epel-release-latest-${EPEL}.noarch.rpm
 echo Downloading common deps :
 yum install wget git tar rpm-build rpmdevtools make -y
-[ ! -z ${ORACLE_OCI_RPM} ] && yum -y --nogpgcheck localinstall ${ORACLE_OCI_RPM}
+[ ! -z ${ORACLE_OCI_RPM} ] && yum -y --nogpgcheck install ${ORACLE_OCI_RPM}
