ecd87cca747afb0a53d8ade9e9a04970b8339fb3
Gregory Boddin <gregory@siwhine.net>
2016-08-23 11:45:28 +0200

    Fix ln mixed target/destination

--- install.sh
+++ install.sh
@@ -8,5 +8,5 @@ yum install wget git tar rpm-build rpmdevtools make -y
 [ ! -z "${THIRD_PARTY_RPMS}" ] && for RPM in ${THIRD_PARTY_RPMS} ; do
   yum -y --nogpgcheck install ${RPM}
   # Fix Oracle instant client missing link
-  echo $RPM|grep -q "^oracle-instantclient12.1-devel" && ln -s /usr/lib/oracle/12.1/client64/lib/libnnz.so /usr/lib/oracle/12.1/client64/lib/libnnz12.so || /bin/true
+  echo $RPM|grep -q "^oracle-instantclient12.1-devel" && ln -s /usr/lib/oracle/12.1/client64/lib/libnnz12.so /usr/lib/oracle/12.1/client64/lib/libnnz.so || /bin/true
 done
