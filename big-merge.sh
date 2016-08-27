80c695b5ae199ddd28f0b17b7ec690909d40a74c
Gregory Boddin <gregory@siwhine.net>
2016-08-27 16:49:37 +0200

    Default to dev repo on big-merge

--- big-merge.sh
+++ big-merge.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
+ORIGIN=$1
+[ -z $1 ] && ORIGIN=dev
 echo "Syncing branches ..."
 ls SPECS/|sed 's/.spec$//g' |grep -v edge-repo|while read soft; do
-  git checkout $soft && git merge master && git push
+  git checkout $soft && git merge master && git push $ORIGIN
   git checkout master && git merge $soft
 done
-git push
+git push $ORIGIN
