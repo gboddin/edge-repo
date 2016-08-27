1431bff7083ee2ce1adc2efe90ca40b75d0af8bd
Gregory Boddin <gregory@siwhine.net>
2016-08-27 15:48:48 +0200

    Added merging script

---
+++ big-merge.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+Syncing branches ...
+ls SPECS/|sed 's/.spec$//g' |grep -v edge-repo|while read soft; do
+  git checkout $soft && git merge master && git push
+  git checkout master && git merge $soft
+done
+git push
