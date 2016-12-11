6385c3ad62a2e2cfee674602a1a80be3dd551db6
Gregory Boddin <gregory@siwhine.net>
2016-12-11 19:16:36 +0100

    Added no-commit option to big-merge

--- big-merge.sh
+++ big-merge.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "Syncing branches ..."
 ls SPECS/|sed 's/.spec$//g' |grep -v edge-repo|while read soft; do
-  git checkout $soft && git merge --squash master 
-  git checkout master && git merge --squash $soft
+  git checkout $soft && git merge master 
+  git checkout master && git merge --no-commit $soft
 done
