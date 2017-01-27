8e2a3e4004ef4a55356ca27ef709d8b1043a370c
Gregory Boddin <gregory@siwhine.net>
2017-01-27 14:46:00 +0100

    Depracted repo

--- big-merge.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-echo "Syncing branches ..."
-ls SPECS/|sed 's/.spec$//g' |grep -v edge-repo|while read soft; do
-  git checkout $soft && git merge master 
-  git checkout master && git merge --no-commit $soft
-done
--- sync_repo.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-rsync --ignore-existing -arvce "ssh -i deploy.key -o StrictHostKeyChecking=no" RPMS/ ${RSYNC_REPO}:/var/www/html/repo/${DISTRO}/RPMS/
-rsync --ignore-existing -arvce "ssh -i deploy.key -o StrictHostKeyChecking=no" SRPMS/ ${RSYNC_REPO}:/var/www/html/repo/${DISTRO}/SRPMS/
